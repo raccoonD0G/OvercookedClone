ad0de9a35cc407d7b8a3c4c124e062c7225578e6
raccoonD0G <w3yfrl@naver.com>
2025-05-16 12:55:14 +0900

    cash

--- Source/OvercookedClone/Actors/CashRegister.h
+++ Source/OvercookedClone/Actors/CashRegister.h
@@ -12,17 +12,5 @@ class OVERCOOKEDCLONE_API ACashRegister : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
-	ACashRegister();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	
-	
+	ACashRegister();	
 };
