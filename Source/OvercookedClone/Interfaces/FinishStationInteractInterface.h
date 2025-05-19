c70d9e0d23efc9adcd94d51e19c52c8e6a470c87
Hite_C <sht6130@gmail.com>
2025-05-19 14:01:21 +0900

    5.19(2)

--- Source/OvercookedClone/Interfaces/FinishStationInteractInterface.h
+++ Source/OvercookedClone/Interfaces/FinishStationInteractInterface.h
@@ -8,7 +8,7 @@
 #include "FinishStationInteractInterface.generated.h"
 
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, BlueprintType)
 class UFinishStationInteractInterface : public UInterface
 {
 	GENERATED_BODY()
